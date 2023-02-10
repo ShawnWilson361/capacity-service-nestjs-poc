b1ea23dc4f43d3effb667ed74439125960236542
Shawn W <shawndawilson1+github@gmail.com>
2023-02-10 09:20:47 +0000

    chore: 🔧 update dependancies

--- src/types/shared/ManagementCapacityChange.dto.ts
+++ src/types/shared/ManagementCapacityChange.dto.ts
@@ -27,14 +27,14 @@ export class ManagementCapacityChange {
   bookingReferenceId?: string;
 
   @ApiProperty({ type: String, nullable: true })
-  @IsDateString({ message: 'Created At is a date string' })
+  @IsDateString({}, { message: 'Created At is a date string' })
   createdAt?: string;
 
   @ApiProperty({ type: String, nullable: true })
-  @IsDateString({ message: 'Updated At is a date string' })
+  @IsDateString({}, { message: 'Updated At is a date string' })
   updatedAt?: string;
 
   @ApiProperty({ type: String, nullable: true })
-  @IsDateString({ message: 'Deleted At is a date string' })
+  @IsDateString({}, { message: 'Deleted At is a date string' })
   deletedAt?: string;
 }
